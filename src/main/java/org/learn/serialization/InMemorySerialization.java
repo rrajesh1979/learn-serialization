4603912de9f7d1896f404ef06065d0ddee3eafff
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-08-01 10:14:20 -0500

    InstrumentationAgent

--- src/main/java/org/learn/serialization/InMemorySerialization.java
+++ src/main/java/org/learn/serialization/InMemorySerialization.java
@@ -7,13 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.StopWatch;
 import org.learn.domain.Account;
 import org.learn.domain.User;
+import org.learn.playground.InstrumentationAgent;
 
 import java.io.*;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class InMemorySerialization {
-    public static final int NUM_ITERATIONS = 100000;
+    public static final int NUM_ITERATIONS = 1;
     static final MetricRegistry metrics = new MetricRegistry();
     static Histogram serializationHistogram = null;
     static Histogram deSerializationHistogram = null;
@@ -27,6 +28,7 @@ public class InMemorySerialization {
         serializationHistogram = metrics.histogram("serializationTime " + Account.class);
         deSerializationHistogram = metrics.histogram("deSerializationTime " + Account.class);
         Account account = new Account("John", "Doe", "1899 Johnstown Road, East Dundee, Illinois, 60118", 10002, 100000.00, "Savings");
+        log.info("Object size of :: {} is :: {}", account.getClass().getName(), InstrumentationAgent.getObjectSize(account));
         for (int i = 0; i < NUM_ITERATIONS; i++) {
             new InMemorySerialization().fileSerialization(account, Account.class);
         }
@@ -36,6 +38,7 @@ public class InMemorySerialization {
         User user = new User();
         user.setId(1);
         user.setName("Mark");
+        log.info("Object size of :: {} is :: {}", user.getClass().getName(), InstrumentationAgent.getObjectSize(user));
         for (int i = 0; i < NUM_ITERATIONS; i++) {
             new InMemorySerialization().fileSerialization(user, User.class);
         }
@@ -66,12 +69,14 @@ public class InMemorySerialization {
             ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
             watch.reset();
             watch.start();
+            User deserializedUser = null;
+            Account deserializedAccount = null;
             switch (ObjClass.getName()) {
-                case "User":
-                    User deserializedUser = (User) objectInputStream.readObject();
+                case "org.learn.domain.User":
+                    deserializedUser = (User) objectInputStream.readObject();
                     break;
-                case "Account":
-                    Account deserializedAccount = (Account) objectInputStream.readObject();
+                case "org.learn.domain.Account":
+                    deserializedAccount = (Account) objectInputStream.readObject();
             }
             objectInputStream.close();
             watch.stop();
